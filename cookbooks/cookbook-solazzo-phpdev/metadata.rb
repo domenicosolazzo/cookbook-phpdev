e2bfee8695fab00384b5d4edba7e4b4e56e5e8ad
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-07-03 19:54:43 +0200

    Added dependencies on metadata.rb

--- cookbooks/cookbook-solazzo-phpdev/metadata.rb
+++ cookbooks/cookbook-solazzo-phpdev/metadata.rb
@@ -1,7 +1,21 @@
 name             'cookbook-solazzo-phpdev'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Domenico Solazzo'
+maintainer_email 'solazzo.domenico@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook-solazzo-phpdev'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "mongodb"
+depends "apache2"
+depends "apt"
+depends "build-essential"
+depends "chef_handler"
+depends "mysql"
+depends "networking_basic"
+depends "openssl"
+depends "php"
+depends "xdebug"
+depends "xml"
+depends "phpunit"
+depends "composer"
